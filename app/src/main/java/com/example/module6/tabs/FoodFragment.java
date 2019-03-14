31e20d63c0777785ab974aa922da008f2c03591e
Mihai Nicolae <mihainicolae2308@gmail.com>
2019-03-14 20:48:36 +0200

    Added code for tabbed application.

---
+++ app/src/main/java/com/example/module6/tabs/FoodFragment.java
@@ -0,0 +1,26 @@
+package com.example.module6.tabs;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.module6.R;
+
+
+public class FoodFragment extends Fragment {
+    public FoodFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_food, container, false);
+    }
+}
